3887a6cb81124d8ba0cdb3698b0be09479e7978c
Ja-Kuba <jjk.kow@gmail.com>
2022-05-21 13:05:15 +0200

    msg

--- email_parser/email_parser.py
+++ email_parser/email_parser.py
@@ -1,48 +1,78 @@
 import email
+import json
 import traceback
 from  tqdm import tqdm
 from email import policy
 from os import listdir
 from os.path import isfile, join
 import traceback
+from spam_preprocessing import EmmailPreprocessing
 
-def getEmailBody(filename):
-    with open(filename, "r", encoding="utf-8", errors='ignore') as f:
-        m_str =  f.read()
-        msg = email.message_from_string(m_str, policy=policy.default)
-        try:
-            body = msg.get_body(('plain',))
-            if body:
-                body = body.get_content()
-            else:
+class EmailParser(EmmailPreprocessing):
+    def __init__(self) -> None:
+        super().__init__()
+    
+
+    def getEmailBody(self, filename):
+        with open(filename, "r", encoding="utf-8", errors='ignore') as f:
+            m_str =  f.read()
+            msg = email.message_from_string(m_str, policy=policy.default)
+            try:
+                body = msg.get_body(('plain',))
+                if body:
+                    body = body.get_content()
+                else:
+                    return False
+            except Exception as e:
+                print(e)
+                print(filename)
                 return False
-        except Exception as e:
-            print(e)
-            print(filename)
-            return False
 
-    return body
+        return body
 
 
-def dumpToFile(filename, text):
-    with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
+    def dumpToFile(self, filename, text):
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(text)
 
+    def readEmailsBody(self, dir_path, max_files:int = -1):
+        bodys = []
+        emails_files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+        for i, ef in tqdm(enumerate(emails_files)):
+            tmp = self.getEmailBody(dir_path + ef)
+            if tmp:
+                bodys.append(tmp)
+                #dumpToFile(f"{out_dir + ef}_body", tmp)
+            if i == max_files: break
 
-def readEmailsBody(dir_path, out_dir):
-    emails_files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
-    for ef in tqdm(emails_files):
-        tmp = getEmailBody(dir_path + ef)
-        if tmp:
-            #dumpToFile(f"{out_dir + ef}_body", tmp)
-            pass
+        return bodys
 
-
+    def buildFreqVec(body):
+        pass
+    
+    def buildEmmbedding(body):
+        pass
 
 # r = getEmailBody("sample2")
 # dumpToFile("out.txt", r)
-SPAM_DIR = 'C:\Projekty\ZUM\zum\corpus\spam\spam\\'
-SPAM_BODY_DIR = 'C:\Projekty\ZUM\zum\corpus\spam\spam_body\\'
+import os
+cwd = os.getcwd()
+print(cwd)
+with open("email_parser\parser_conf.json", "r", encoding="utf-8") as f:
+    confg = json.loads(f.read())
+     
+#SPAM_DIR = 'C:\Projekty\ZUM\zum\corpus\spam\spam\\'
+#SPAM_BODY_DIR = 'C:\Projekty\ZUM\zum\corpus\spam\spam_body\\'
+EmmailPreprocessing.downloadNLTKPackages()
 
-readEmailsBody(SPAM_DIR, SPAM_BODY_DIR)
+
+ep = EmailParser()
+bodys = ep.readEmailsBody(confg["spam_dir"], max_files = 1 )
+
+print(bodys)
+
+
+
+
+
 
