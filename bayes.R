f62425ed16c04d30fd4a501a02033b82b4776af9
Ja-Kuba <jjk.kow@gmail.com>
2022-06-01 22:50:06 +0200

    bayes test_function

--- bayes.R
+++ bayes.R
@@ -23,10 +23,15 @@ model_bayes <- function(x_train_arg, y_train_arg, laplace_arg = 0){
 }
 
 
-test_bayes <- function(dataset, laplace_arg = 0){
-  iters = seq(1, 2, by=1)
-
+test_bayes <- function(dataset, iters_arg = 2, laplace_arg = 0){
+  
+  pr_prob_all = c()
+  pr_class_all = c()
+  y_test_all = c()
+  
+  iters = seq(1, iters_arg, by=1)
   for(i in iters){
+    print(i)
     #genrate new split
     d_sets = splitTrainTest(dataset)
     x_train = d_sets[[1]]
@@ -37,37 +42,51 @@ test_bayes <- function(dataset, laplace_arg = 0){
     gnb = model_bayes(x_train, y_train)
     pr_class = predict(gnb, newdata = data.matrix(x_test), type = "class")
     pr_prob = predict(gnb, newdata = data.matrix(x_test), type = "prob")
+
+    pr_prob_tmp <- as.data.frame(pr_prob) 
+    pr_prob_spam = pr_prob_tmp$spam
+      
+    pr_class_all <- c(pr_class_all, pr_class)
+    pr_prob_all <- c(pr_prob_all, pr_prob_spam)
+    y_test_all <- c(y_test_all, y_test)
     
   }
   
+  
+  print(table(y_test_all, pr_class_all))
+  #metrics
+  cm = as.matrix(table(y_test_all, pr_class_all))
+  calculateMetrics(cm)
+  
+  #ROC
+  getROC(pr_prob_all, y_test_all)
 }
 
-test_bayes(data_embbedings)
+test_bayes(data_embbedings, iters_arg = 10)
 
 
-
-#train model
-gnb <- gaussian_naive_bayes(x = data.matrix(x_train), y = y_train, laplace=2)
-#summary(gnb)
-
-
-#test on training data
-pr_train = predict(gnb, newdata = data.matrix(x_train), type = "class")
-table(y_train, pr_train)
-
-#test on testing data
-pr_test = predict(gnb, newdata = data.matrix(x_test), type = "class")
-pr_test_prob = predict(gnb, newdata = data.matrix(x_test), type = "prob")
-
-cm = as.matrix(table(y_test, pr_test))
-calculateMetrics(cm)
-
-
-#ROC 
-pr_prob_tmp <- as.data.frame(pr_test_prob) 
-pr_prob_spam = pr_prob_tmp$spam
-getROC(pr_prob_spam, y_test)
-
+##train model
+#gnb <- gaussian_naive_bayes(x = data.matrix(x_train), y = y_train, laplace=2)
+##summary(gnb)
+#
+#
+##test on training data
+#pr_train = predict(gnb, newdata = data.matrix(x_train), type = "class")
+#table(y_train, pr_train)
+#
+##test on testing data
+#pr_test = predict(gnb, newdata = data.matrix(x_test), type = "class")
+#pr_test_prob = predict(gnb, newdata = data.matrix(x_test), type = "prob")
+#
+#cm = as.matrix(table(y_test, pr_test))
+#calculateMetrics(cm)
+#
+#
+##ROC 
+#pr_prob_tmp <- as.data.frame(pr_test_prob) 
+#pr_prob_spam = pr_prob_tmp$spam
+#getROC(pr_prob_spam, y_test)
+#
 
 
 
